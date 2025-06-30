bb3fbc61af2cde672cda7cd27238e2c2b1a306da
tadj <tadj.d.cazaubon@gmail.com>
2025-06-30 20:13:10 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,61 @@
+from discord import (
+    Intents,
+    Message
+)
+
+from discord.ext import (
+    commands
+)
+
+from logging import(
+    Logger,
+    FileHandler,
+    Formatter,
+    getLogger,
+    DEBUG
+)
+
+from pathlib import Path
+from os import environ
+from dotenv import load_dotenv
+
+
+LOGFILE:Path = Path(__file__).parent.resolve() / "discord.log"
+HANDLER = FileHandler(filename=LOGFILE, encoding='utf-8', mode='w')
+HANDLER.setFormatter(Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"))
+
+load_dotenv()
+TOKEN = environ.get("TOKEN", None)
+
+intents = Intents.default()
+intents.presences = True
+intents.message_content = True
+intents.members = True
+
+KACK = "KACK "
+
+orca = commands.Bot(command_prefix=KACK, intents=intents)
+
+@orca.event
+async def on_ready() -> None:
+    print(f"We are ready to rumble on {orca.user.name}")
+
+@orca.command()
+async def who(context: commands.Context):
+    message = context.message.content
+    substringbeg = message.index(KACK)
+    await context.send(f"Command is: {message[substringbeg+4:]}")
+
+@orca.command()
+async def what(context: commands.Context):
+    message = context.message.content
+    substringbeg = message.index(KACK)
+    await context.send(f"Command is: {message[substringbeg+4:]}")
+
+
+orca.run(
+    TOKEN,
+    log_handler=HANDLER,
+    log_formatter=Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"),
+    log_level=DEBUG
+)
